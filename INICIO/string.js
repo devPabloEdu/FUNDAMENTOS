2b45038cbb600cb1cd375ac09b1d111149270659
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-04-28 09:29:51 -0300

    att

---
+++ INICIO/string.js
@@ -0,0 +1,7 @@
+const escola = 'cod3r';
+console.log(escola.charAt(4));
+console.log(escola.charCodeAt(3));
+console.log(escola.indexOf('d'));
+
+console.log(escola.substring(1));
+console.log(escola.replace(3, 'e'));
