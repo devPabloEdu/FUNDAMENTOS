c53caad0f573e817ef704ad0d9827e401d0cf77b
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-03 17:16:03 -0300

    att

---
+++ INICIO/null.js
@@ -0,0 +1,10 @@
+const a = {name: 'teste'};
+const b = a;
+b.name = 'opa';
+console.log(a.name);
+
+let c = 3;
+let d = c;
+d++
+
+
---
+++ INICIO/nullndefined.js
@@ -0,0 +1,19 @@
+let valor //não atribuida, ou nao inicializada
+console.log(valor);
+
+valor = null; //ausencia de valor
+console.log(valor);
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.peco = 3.50
+console.log(produto);
+console.log(!!produto.preco);
+
+produto.preco = null;
+console.log(!!produto.preco);
+console.log(produto);
+
+
