de70f3b98ca3e4f477f2b3d1802bb5b4e13e23ba
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-03 16:13:24 -0300

    ATT

--- INICIO/objeto.js
+++ INICIO/objeto.js
@@ -2,3 +2,10 @@ const prod1 = {};
 prod1.nome = 'celular ultra mega';
 prod1.preco = 45.10;
 console.log(prod1);
+
+const prod2 = {
+    nome : 'camiseta',
+    preco : 50.00,
+    cor : 'preta'
+};
+console.log(prod2);
