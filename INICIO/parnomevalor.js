e3ea45716048fc67124dcff122f6cca3236e1edc
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-08 22:49:23 -0300

    att

---
+++ INICIO/parnomevalor.js
@@ -0,0 +1,22 @@
+//par nome/valor
+const saudacao = 'opa'; // contexto lexico 1;
+
+function exec(){
+    const saudacao = 'falaaa'; // contexto lexico 2;
+    return saudacao;
+}
+console.log(saudacao);
+
+//objetos sao grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'pedro',
+    idade: 32,
+    peso: 90,
+    endereço: {
+        logradouro: 'rua muito legal',
+        numero : 123
+    }
+}
+console.log(saudacao);
+console.log(exec())
+console.log(cliente);
