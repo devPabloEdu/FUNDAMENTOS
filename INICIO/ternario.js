ef8c3b0356f2dd626886707f1a52bd6ede62b068
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-13 17:55:23 -0300

    att

---
+++ INICIO/ternario.js
@@ -0,0 +1,2 @@
+const resultado = nota => nota >= 7 ? 'aprovado' : 'reprovador'
+console.log(resultado(8)); // defini o parametro de nota
---
+++ INICIO/unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1;
+let num2 = 2;
+
+num1++ //pósfixada, ele acrescenta uma unidade ao valor da variavel
+console.log(num1);
+
+--num1//préfixada, ele substitui uma unidade da variavel
+console.log(num1);
+
+console.log(++num1 === num2--); //da true pq a adição foi executado antes de comparar
