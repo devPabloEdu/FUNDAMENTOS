3a3508eb8e80ae1f84f9a70d042ef9081f1ddb5c
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-17 16:31:07 -0300

    att

--- CONTROLE/if2.js
+++ CONTROLE/if2.js
@@ -6,3 +6,13 @@ function teste1(num){
 
 teste1(6);
 teste1(8);
+
+function teste2(num){
+    if(num > 7); // blovo nao esta vinculado ao if, pois o ponto e virgula termina a sentença nessa posição
+    {
+        console.log(num)
+    }
+}
+
+teste2(6);
+teste2(8);
